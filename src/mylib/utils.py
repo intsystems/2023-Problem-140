d98bfba3a343ecae414496b1f8d82efa4bc8d6a7
BorONE <saveliibor11@gmail.com>
2023-04-19 04:15:30 +0300

    Add comments

--- src/mylib/utils.py
+++ src/mylib/utils.py
@@ -5,7 +5,16 @@ ACC, LOSS, LAT = {'accuracy'}, {'loss'}, {'latency'}
 ALL = ACC | LOSS | LAT
 
 @torch.no_grad()
-def validate(model, getters: set=ALL, *, dl, loss_fn, times, device):
+def validate(model, getters: set=ALL, *, dl, loss_fn=None, times=None, device):
+    """
+    :param model:
+    :param getters: subset of {'accuracy','loss','latency'}
+    :param dl: valid dataloader
+    :param loss_fn: used to calculate loss
+    :param times: used to calculate latency of backbone
+    :param device:
+    :return: average accuracy, loss and latency if corresponding getter is passed
+    """
     n_true, n_tot = 0, 0
     loss, latency = 0, 0
 
@@ -28,6 +37,16 @@ def validate(model, getters: set=ALL, *, dl, loss_fn, times, device):
 
 
 def train_epoch(hypernet, backbone, optimizer, *, sample_lambd, dl, loss_fn, times, device):
+    """
+    :param hypernet:
+    :param backbone:
+    :param optimizer:
+    :param sample_lambd:
+    :param dl: train dataloader
+    :param loss_fn:
+    :param times:
+    :param device:
+    """ 
     for i, (X, y) in enumerate(dl):
         if X.shape[0] != 64:
             continue
@@ -44,7 +63,15 @@ def train_epoch(hypernet, backbone, optimizer, *, sample_lambd, dl, loss_fn, tim
         optimizer.step()
 
 
-def add_report(reports, discrete, backbone, **validate_kwargs):
+def add_report(reports, discrete: bool, backbone, **validate_kwargs):
+    """
+    Adds validate result to reports
+
+    :param reports: dicts with keys 'acc', 'loss', 'lat' and values [...]
+    :param discrete: validating on discrete backbone if True
+    :param backbone: backbone model
+    :param **validate_kwargs: passed to validate function after backbone
+    """ 
     if discrete:
         backbone.make_gammas_discrete()
 
@@ -60,6 +87,13 @@ def add_report(reports, discrete, backbone, **validate_kwargs):
 
 
 def one_hot(i: int, n: int):
+    """
+    one_hot(2, 4) returns tensor([[ 0., 0., 1., 0])
+
+    :param i: position of 1
+    :param n: size of vector
+    :return: one hot vector filled with 0. and single 1.
+    """ 
     result = [0.] * n
     result[i] = 1
     return torch.tensor(result)
