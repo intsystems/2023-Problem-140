e8386f7c603809b1c4c67509cb08f1f927286fee
Savely Borodin <saveliibor-1@mail.ru>
2023-08-22 00:22:58 +0300

    test

--- src/mylib/model.py
+++ src/mylib/model.py
@@ -8,12 +8,12 @@ class ResNet18(nn.Module):
     def __init__(self, *args, **kwargs):
         num_classes: int = 10
         super(ResNet18, self).__init__()
-        self.selected_output = {}
+        # self.selected_output = {}
         self.model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
         self.model.fc = nn.Linear(512, num_classes)
-        self.fhooks = []
-        for layer in self.model._modules.keys():
-            self.fhooks.append(getattr(self.model, layer).register_forward_hook(self.forward_hook(layer)))
+        # self.fhooks = []
+        # for layer in self.model._modules.keys():
+        #     self.fhooks.append(getattr(self.model, layer).register_forward_hook(self.forward_hook(layer)))
 
     def forward_hook(self, layer_name):
         def hook(module, inp, out):
