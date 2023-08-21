ec7a14bca27ee9feb096747dced69e1df6ebcf4c
Savely Borodin <saveliibor-1@mail.ru>
2023-08-22 00:03:26 +0300

    Always use ResNet18_Weights

--- src/mylib/model.py
+++ src/mylib/model.py
@@ -11,13 +11,11 @@ class ResNet18(nn.Module):
 
     def init_for(self, dataset_name, path_to_model=None):
         assert dataset_name in {'CIFAR10', 'CIFAR100', 'ImageNet'}
-        if dataset_name == 'ImageNet':
-            self.model = resnet18(weights=ResNet18_Weights.DEFAULT)
-        else:
+        self.model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        if dataset_name != 'ImageNet':
             assert path_to_model != None
-            self.model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
-            num_classes = 10 if dataset_name == 'CIFAR10' else 100
-            self.model.fc = nn.Linear(512, num_classes)
+            num_classes = {'CIFAR10':10, 'CIFAR100':100}
+            self.model.fc = nn.Linear(512, num_classes[dataset_name])
             self.model.load_state_dict(torch.load(path_to_model))
 
     def forward(self, x):
